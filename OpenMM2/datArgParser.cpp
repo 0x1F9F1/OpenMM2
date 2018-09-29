02518c7af99016463acd073b720795001a5eea5e
Brick <samuelwilde@hotmail.com>
2018-09-29 15:25:40 +0100

    Implemented datArgParser::[Exists/Get] functions

--- OpenMM2/datArgParser.cpp
+++ OpenMM2/datArgParser.cpp
@@ -18,22 +18,69 @@ struct ArgEntry
 
 bool datArgParser::Exists(const char * name)
 {
-    return stub<cdecl_t<bool, const char*>>(0x4C6190, name);
+    if (!Argc)
+    {
+        Init();
+    }
+
+    return ArgHash.Access(name) != nullptr;
 }
 
 bool datArgParser::Get(const char * name, uint32_t index, int & out)
 {
-    return stub<cdecl_t<bool, const char*, uint32_t, int&>>(0x4C61C0, name, index, out);
+    if (!Argc)
+    {
+        Init();
+    }
+
+    ArgEntry* entry = (ArgEntry*) ArgHash.Access(name);
+
+    if (entry && index < entry->WordCount)
+    {
+        out = atoi(entry->Words[index]);
+
+        return true;
+    }
+
+    return false;
 }
 
 bool datArgParser::Get(const char * name, uint32_t index, float & out)
 {
-    return stub<cdecl_t<bool, const char*, uint32_t, float&>>(0x4C6210, name, index, out);
+    if (!Argc)
+    {
+        Init();
+    }
+
+    ArgEntry* entry = (ArgEntry*) ArgHash.Access(name);
+
+    if (entry && index < entry->WordCount)
+    {
+        out = (float) atof(entry->Words[index]);
+
+        return true;
+    }
+
+    return false;
 }
 
 bool datArgParser::Get(const char * name, uint32_t index, const char *& out)
 {
-    return stub<cdecl_t<bool, const char*, uint32_t, const char*&>>(0x4C6260, name, index, out);
+    if (!Argc)
+    {
+        Init();
+    }
+
+    ArgEntry* entry = (ArgEntry*) ArgHash.Access(name);
+
+    if (entry && index < entry->WordCount)
+    {
+        out = entry->Words[index];
+
+        return true;
+    }
+
+    return false;
 }
 
 void datArgParser::Init()
--- OpenMM2/datArgParser.h
+++ OpenMM2/datArgParser.h
@@ -6,6 +6,7 @@ class datArgParser
 {
 public:
     static bool Exists(const char* name);
+
     static bool Get(const char* name, uint32_t index, int& out);
     static bool Get(const char* name, uint32_t index, float& out);
     static bool Get(const char* name, uint32_t index, const char*& out);
